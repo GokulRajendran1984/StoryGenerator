ae1780c4de067f47b9a42865757f0c6e0fe84355
GokulRajendran1984 <gookulr@gmail.com>
2025-03-22 18:25:55 -0400

    Create prompt_generator.py

---
+++ prompt_generator.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+  
+# Streamlit UI
+st.title("Creative Writing")
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+
+# Input fields
+topic = st.text_input("Enter topic:")
+genre = st.selectbox("Select genre:", ["Science Fiction", "Fantasy", "Mystery", "Romance", "Thriller"])
+length = st.slider("Select prompt length (words):", min_value=50, max_value=10000, step=10, value=100)
+generate_button = st.button("Generate Story")
+concatenated_content = ""
+
+     
+# Validation api_key
+if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+if generate_button and openai_api_key.startswith('sk-'):
+    # Generate prompt on button click
+    if generate_button:
+        if topic:
+            st.write("Generating Story...")                
+            prompt=f"Write a {genre} story about {topic}"
+            llm=ChatOpenAI(api_key=openai_api_key,temperature=0.8,model_name="gpt-3.5-turbo")
+            response=llm.stream(prompt)
+            for chunk in response:
+                content_without_timestamps = ''.join(filter(lambda x: not x.isdigit(), chunk.content))
+                concatenated_content += content_without_timestamps
+                if len(concatenated_content) >= length:
+                    last_full_stop_index = concatenated_content[:length].rfind('.')
+                    if last_full_stop_index != -1:
+                        concatenated_content = concatenated_content[:last_full_stop_index + 1]
+                    else:
+                        concatenated_content = concatenated_content[:length]
+                    break 
+            st.write(f"{concatenated_content}")
+        else:
+            st.error("Please enter a topic.")
